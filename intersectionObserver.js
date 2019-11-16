69f05b7704e07d4454a7042379013ad9ba58bcb3
EdgeworthWright <42786499+EdgeworthWright@users.noreply.github.com>
2019-11-16 14:07:36 +0100

    tweede video

--- intersectionObserver.js
+++ intersectionObserver.js
@@ -1,5 +1,16 @@
 let alleLinks = document.querySelectorAll('nav ul li a');
+let deSecties = document.querySelectorAll('section');
 
+const opties = {};
+const verwerkDoorsnijding = (entries, observer) => {
+    entries.forEach( entry => {
+        console.log(entry.target + " doorsnijdt " + entry.isIntersecting);
+    });
+}
+
+let observer = new IntersectionObserver(verwerkDoorsnijding, opties);
+
+observer.observe(deSecties[1]);
 
 // Functie die active verwijderd
 const verwijderActief = () => {
